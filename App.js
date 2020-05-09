e357b58034a02a921fdb2aa5b8378c02d8b17223
memed <rafik_ahmed@outlook.fr>
2020-05-09 12:29:15 +0200

    add navigation

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import Login from "./components/Login";
+import Register from "./components/Register";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Login/>
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login" headerMode="none">
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Register" component={Register} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#3C7EA3',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
